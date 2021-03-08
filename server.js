f423d161771596d6827d7dbb84dca6d604b3df5e
Curtis Smith <cj1988333@hotmail.com>
2021-03-07 22:24:50 -0500

    seeding Heroku

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const seedAll = require('./seeds/index');
 const routes = require('./routes');
 // import sequelize connection
 const sequelize = require('./config/connection');
+
+seedAll()
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,3 +17,5 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
+
+
