7285ec3bf54deef8960c2fcf557cca930e16b6c5
Curtis Smith <cj1988333@hotmail.com>
2021-03-07 19:20:56 -0500

    finished category routes

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ app.use(routes);
 // });
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
